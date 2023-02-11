af677b21978555cf676ada681d5a169c2615bedf
Youns <youns.s.ibrahim115@gmail.com>
2023-02-11 21:31:07 +0300

    add carousel

--- views/partials/gallery.php
+++ views/partials/gallery.php
@@ -1,4 +1,4 @@
-<div id="carouselExampleCaptions" class="carousel slide relative " data-bs-ride="carousel">
+<div id="carouselExampleCaptions" class="carousel slide relative outline-text-black" data-bs-ride="carousel">
   <div class="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4">
     <button
       type="button"
@@ -22,37 +22,37 @@
     ></button>
   </div>
   <div class="carousel-inner relative w-full overflow-hidden ">
-    <div class="carousel-item active relative float-left w-full h-90" >
+    <div class="carousel-item active relative float-left w-full " >
       <img
-        src="https://mdbootstrap.com/img/Photos/Slides/img%20(15).jpg"
+        src="/images/uni-1.jpg"
         class="block w-full h-full object-cover "
-        alt="..."
+        alt="university picture-1"
+      />
+      <div class="carousel-caption hidden md:block absolute text-center  ">
+        <h5 class="text-xl ">First</h5>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe.</p>
+      </div>
+    </div>
+    <div class="carousel-item relative float-left w-full ">
+      <img
+        src="/images/uni-2.jpg"
+        class="block w-full h-full object-cover"
+        alt="university picture-2"
       />
       <div class="carousel-caption hidden md:block absolute text-center">
-        <h5 class="text-xl">First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
+        <h5 class="text-xl">Second</h5>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi!</p>
       </div>
     </div>
     <div class="carousel-item relative float-left w-full">
       <img
-        src="https://mdbootstrap.com/img/Photos/Slides/img%20(22).jpg"
-        class="block w-full"
-        alt="..."
+        src="/images/uni-3.jpg"
+        class="block w-full h-full object-cover"
+        alt="university picture-3"
       />
-      <div class="carousel-caption hidden md:block absolute text-center">
-        <h5 class="text-xl">Second slide label</h5>
-        <p>Some representative placeholder content for the second slide.</p>
-      </div>
-    </div>
-    <div class="carousel-item relative float-left w-full">
-      <img
-        src="https://mdbootstrap.com/img/Photos/Slides/img%20(23).jpg"
-        class="block w-full"
-        alt="..."
-      />
-      <div class="carousel-caption hidden md:block absolute text-center">
-        <h5 class="text-xl">Third slide label</h5>
-        <p>Some representative placeholder content for the third slide.</p>
+      <div class="carousel-caption hidden md:block absolute text-center ">
+        <h5 class="text-xl ">Third</h5>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste.</p>
       </div>
     </div>
   </div>
@@ -75,3 +75,26 @@
     <span class="visually-hidden">Next</span>
   </button>
 </div>
+
+
+<style>
+
+  /* resize images (Tailwind'a suk eshy nadakrd)  */
+  @media screen and (min-width: 800px){ 
+    
+    .carousel-item{
+      height: 80vh;
+    }
+      
+     .carousel-item img{
+      height: 100%;
+    }
+    
+   }
+    
+   .outline-text-black{ 
+    text-shadow: rgb(0, 0, 0) 1px 0px 0px, rgb(0, 0, 0) 0.540302px 0.841471px 0px, rgb(0, 0, 0) -0.416147px 0.909297px 0px, rgb(0, 0, 0) -0.989993px 0.14112px 0px, rgb(0, 0, 0) -0.653644px -0.756803px 0px, rgb(0, 0, 0) 0.283662px -0.958924px 0px, rgb(0, 0, 0) 0.96017px -0.279416px 0px;
+   }
+
+
+    </style>
