5b3b85c15c908355196eb10ff22b2799df2f5f62
youns <youns.s.ibrahim115@gmail.com>
2023-03-27 12:29:00 +0300

    fix embedded map

--- views/faq.view.php
+++ views/faq.view.php
@@ -4,71 +4,7 @@
 <main>
     <?php require('partials/banner.php') ?>
 
-    <div class="mx-auto lg:mx-20 mt-7" id="accordion-color" data-accordion="collapse" data-active-classes="bg-blue-100 dark:bg-gray-800 text-blue-600 dark:text-white">
-        <div class="mx-4 sm:mx-0">
-            <h2 id="accordion-color-heading-1">
-                <button type="button" class="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-800 dark:border-gray-700 dark:text-gray-400 hover:bg-blue-100 dark:hover:bg-gray-800" data-accordion-target="#accordion-color-body-1" aria-expanded="true" aria-controls="accordion-color-body-1">
-                    <span>Lorem ipsum dolor sit amet?</span>
-                    <svg data-accordion-icon class="w-6 h-6 rotate-180 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
-                    </svg>
-                </button>
-            </h2>
-            <div id="accordion-color-body-1" class="hidden" aria-labelledby="accordion-color-heading-1">
-                <div class="p-5 font-light border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
-                    <p class="mb-2 text-gray-500 dark:text-gray-400">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus illo voluptatibus voluptate qui odio. Totam quia neque ipsa fugiat rerum?</p>
-                </div>
-            </div>
-            <h2 id="accordion-color-heading-2">
-                <button type="button" class="flex items-center justify-between w-full p-5 font-medium text-FAleft text-gray-500 border border-b-0 border-gray-200 focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-800 dark:border-gray-700 dark:text-gray-400 hover:bg-blue-100 dark:hover:bg-gray-800" data-accordion-target="#accordion-color-body-2" aria-expanded="false" aria-controls="accordion-color-body-2">
-                    <span>Lorem ipsum dolor sit amet consectetur adipisicing?</span>
-                    <svg data-accordion-icon class="w-6 h-6 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
-                    </svg>
-                </button>
-            </h2>
-            <div id="accordion-color-body-2" class="hidden" aria-labelledby="accordion-color-heading-2">
-                <div class="p-5 font-light border border-b-0 border-gray-200 dark:border-gray-700">
-                    <p class="mb-2 text-gray-500 dark:text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure ea consequatur commodi officiis fuga consequuntur voluptates impedit sunt, culpa quaerat. Minima illum tenetur error quo</p>
-                </div>
-            </div>
-            <h2 id="accordion-color-heading-3">
-                <button type="button" class="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-gray-200 focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-800 dark:border-gray-700 dark:text-gray-400 hover:bg-blue-100 dark:hover:bg-gray-800" data-accordion-target="#accordion-color-body-3" aria-expanded="false" aria-controls="accordion-color-body-3">
-                    <span>Lorem, ipsum dolor sit amet consectetur adipisicing elit?</span>
-                    <svg data-accordion-icon class="w-6 h-6 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
-                    </svg>
-                </button>
-            </h2>
-            <div id="accordion-color-body-3" class="hidden" aria-labelledby="accordion-color-heading-3">
-                <div class="p-5 font-light border border-t-0 border-gray-200 dark:border-gray-700">
-                    <p class="mb-2 text-gray-500 dark:text-gray-400">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta ut adipisci minima mollitia facere accusamus optio, atque error!</p>
-                    <p class="mb-2 text-gray-500 dark:text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores amet vel et, molestias obcaecati aspernatur laudantium nam, praesentium ipsam totam veniam harum nulla cum iure incidunt minima.</p>
-                    <p class="mb-2 text-gray-500 dark:text-gray-400">Lorem ipsum dolor sit amet consectetur:</p>
-                    <ul class="pl-5 text-gray-500 list-disc dark:text-gray-400">
-                        <li><a href="#" class="text-blue-600 dark:text-blue-500 hover:underline">Lorem ipsum dolor</a></li>
-                        <li><a href="#" rel="nofollow" class="text-blue-600 dark:text-blue-500 hover:underline">Lorem, ipsum</a></li>
-                    </ul>
-                </div>
-            </div>
-
-
-            <h2 id="accordion-color-heading-4">
-                <button type="button" class="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-800 dark:border-gray-700 dark:text-gray-400 hover:bg-blue-100 dark:hover:bg-gray-800" data-accordion-target="#accordion-color-body-4" aria-expanded="true" aria-controls="accordion-color-body-4">
-                    <span>Lorem ipsum dolor sit amet Lorem ipsum dolor?</span>
-                    <svg data-accordion-icon class="w-6 h-6 rotate-180 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
-                    </svg>
-                </button>
-            </h2>
-            <div id="accordion-color-body-4" class="hidden" aria-labelledby="accordion-color-heading-4">
-                <div class="p-5 font-light border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
-                    <p class="mb-2 text-gray-500 dark:text-gray-400">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus illo voluptatibus voluptate qui odio. Totam quia neque ipsa fugiat rerum? Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, reiciendis.</p>
-                </div>
-            </div>
-
-        </div>
-    </div>
+    <?php require('partials/faq.php') ?>
 
 
 
--- views/partials/contacts.php
+++ views/partials/contacts.php
@@ -10,14 +10,9 @@
                 <div class="overflow-hidden bg-white rounded-xl">
                     <div class="p-6">
                         <svg class="flex-shrink-0 w-10 h-10 mx-auto text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="1"
-                                d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
-                            />
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                         </svg>
-                        <p class="mt-6 text-lg font-medium text-gray-900">0750-975-45-45</p>
+                        <p class="mt-5 text-lg font-medium text-gray-900">0750-975-45-45</p>
                         <p class="mt-1 text-lg font-medium text-gray-900">0750-976-45-45</p>
                     </div>
                 </div>
@@ -27,29 +22,22 @@
                         <svg class="flex-shrink-0 w-10 h-10 mx-auto text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                         </svg>
-                        <p class="mt-6 text-lg font-medium text-gray-900">info@aynda.edu.krd</p>
+                        <p class="mt-5 text-lg font-medium text-gray-900">info@aynda.edu.krd</p>
                     </div>
                 </div>
 
             </div>
-            
+
             <div class="overflow-hidden bg-white rounded-xl mt-6 ">
-    <div class="p-6">
-        <svg class="flex-shrink-0 w-10 h-10 mx-auto text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
-        </svg>
-        <p class="mt-6 text-lg font-medium leading-relaxed text-gray-900 text-center ">Erbil, Kasnazan Road</p>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1609.7174962157474!2d44.12019812969948!3d36.20462065828428!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x400726d37b703063%3A0xe5bd7fce18249897!2sAynda%20Private%20Technical%20Institute!5e0!3m2!1sen!2sus!4v1679851170501!5m2!1sen!2sus" class="w-full" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-    </div>
-</div>
-
-
-
-
-
-
-
+                <div class="p-6">
+                    <svg class="flex-shrink-0 w-10 h-10 mx-auto text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
+                    </svg>
+                    <p class="mt-5 mb-4 text-lg font-medium leading-relaxed text-gray-900 text-center ">Erbil, Kasnazan Road</p>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1609.7174962157474!2d44.12019812969948!3d36.20462065828428!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x400726d37b703063%3A0xe5bd7fce18249897!2sAynda%20Private%20Technical%20Institute!5e0!3m2!1sen!2sus!4v1679851170501!5m2!1sen!2sus" class="w-full" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+            </div>
 
             <div class="mt-6 overflow-hidden bg-white rounded-xl">
                 <div class="px-6 py-12 sm:p-12">
@@ -80,15 +68,15 @@
 
                             <div class="selectable-option-group">
                                 <label for="city" class="text-base font-medium text-gray-900">Select a City:</label>
-                                    <select id="city" name="city" class="mt-2.5 relative block w-full px-4 py-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md focus:outline-none focus:border-blue-600 caret-blue-600">
-                                        <option value="Erbil">Erbil</option>
-                                        <option value="Sulaymaniyah">Sulaymaniyah</option>
-                                        <option value="Duhok">Duhok</option>
-                                        <option value="Kerkuk">Kerkuk</option>
-                                    </select>
+                                <select id="city" name="city" class="mt-2.5 relative block w-full px-4 py-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md focus:outline-none focus:border-blue-600 caret-blue-600">
+                                    <option value="Erbil">Erbil</option>
+                                    <option value="Sulaymaniyah">Sulaymaniyah</option>
+                                    <option value="Duhok">Duhok</option>
+                                    <option value="Kerkuk">Kerkuk</option>
+                                </select>
                             </div>
 
-                            
+
 
                             <div class="sm:col-span-2">
                                 <label for="" class="text-base font-medium text-gray-900"> Message </label>
@@ -109,7 +97,3 @@
         </div>
     </div>
 </section>
-
-
-
-
