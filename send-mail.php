a8733d033621bd08019c3f7734bdf87d7de6367c
youns <youns.s.ibrahim115@gmail.com>
2023-04-01 02:45:10 +0300

    add form submit via email

---
+++ send-mail.php
@@ -0,0 +1,6 @@
+<?php
+$fullName = $_POST["fullName"];
+$email = $_POST["email"];
+$phone = $_POST["phone"];
+$city = $_POST["city"];
+$message = $_POST["message"];
--- views/partials/contacts.php
+++ views/partials/contacts.php
@@ -43,26 +43,26 @@
                 <div class="px-6 py-12 sm:p-12">
                     <h3 class="text-3xl font-semibold text-center text-gray-900">Send us a message</h3>
 
-                    <form action="#" method="POST" class="mt-14">
+                    <form action="https://formsubmit.co/b65f87f3944a6eb598f2d6ca5a75ff29" method="POST" class="mt-14">
                         <div class="grid grid-cols-1 sm:grid-cols-2 gap-x-5 gap-y-4">
                             <div>
                                 <label for="" class="text-base font-medium text-gray-900"> Your name </label>
                                 <div class="mt-2.5 relative">
-                                    <input type="text" name="" id="" placeholder="Enter your full name" class="block w-full px-4 py-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md focus:outline-none focus:border-blue-600 caret-blue-600" />
+                                    <input type="text" name="fullName" id="fullName" required placeholder="Enter your full name" class="block w-full px-4 py-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md focus:outline-none focus:border-blue-600 caret-blue-600" />
                                 </div>
                             </div>
 
                             <div>
                                 <label for="" class="text-base font-medium text-gray-900"> Email address </label>
                                 <div class="mt-2.5 relative">
-                                    <input type="email" name="" id="" placeholder="Enter your Email address" class="block w-full px-4 py-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md focus:outline-none focus:border-blue-600 caret-blue-600" />
+                                    <input type="email" name="email" id="email" required placeholder="Enter your Email address" class="block w-full px-4 py-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md focus:outline-none focus:border-blue-600 caret-blue-600" />
                                 </div>
                             </div>
 
                             <div>
                                 <label for="" class="text-base font-medium text-gray-900"> Phone number </label>
                                 <div class="mt-2.5 relative">
-                                    <input type="tel" name="" id="" placeholder="Enter your phone number" class="block w-full px-4 py-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md focus:outline-none focus:border-blue-600 caret-blue-600" />
+                                    <input type="tel" name="phone" id="phone" placeholder="Enter your phone number" class="block w-full px-4 py-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md focus:outline-none focus:border-blue-600 caret-blue-600" />
                                 </div>
                             </div>
 
@@ -81,16 +81,19 @@
                             <div class="sm:col-span-2">
                                 <label for="" class="text-base font-medium text-gray-900"> Message </label>
                                 <div class="mt-2.5 relative">
-                                    <textarea name="" id="" placeholder="" class="block w-full px-4 py-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md resize-y focus:outline-none focus:border-blue-600 caret-blue-600" rows="4"></textarea>
+                                    <textarea name="message" id="message" placeholder="" class="block w-full px-4 py-4 text-black placeholder-gray-500 transition-all duration-200 bg-white border border-gray-200 rounded-md resize-y focus:outline-none focus:border-blue-600 caret-blue-600" rows="4"></textarea>
                                 </div>
                             </div>
 
                             <div class="sm:col-span-2">
-                                <button type="submit" class="inline-flex items-center justify-center w-full px-4 py-4 mt-2 text-base font-semibold text-white transition-all duration-200 bg-blue-600 border border-transparent rounded-md focus:outline-none hover:bg-blue-700 focus:bg-blue-700">
+                                <button type="submit" id="submit" class="inline-flex items-center justify-center w-full px-4 py-4 mt-2 text-base font-semibold text-white transition-all duration-200 bg-blue-600 border border-transparent rounded-md focus:outline-none hover:bg-blue-700 focus:bg-blue-700">
                                     Send
                                 </button>
                             </div>
                         </div>
+                        <input type="hidden" name="_subject" value="New Aynda contact">
+                        <input type="hidden" name="_captcha" value="true">
+                        <input type="hidden" name="_autoresponse" value="Thanks for your message we will be contacting you soon :)">
                     </form>
                 </div>
             </div>
